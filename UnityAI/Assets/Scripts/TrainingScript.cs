2d06c47eb234033524dd2a18580b641d406628b9
Adam Stack <bigstack80@knights.ucf.edu>
2015-03-18 19:49:33 -0400

    printing to file sensor data

--- UnityAI/Assets/Scripts/TrainingScript.cs
+++ UnityAI/Assets/Scripts/TrainingScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class TrainingScript : MonoBehaviour {
 
@@ -9,20 +10,25 @@ public class TrainingScript : MonoBehaviour {
 	public float agentDist;
 	public float agentAngle;
 	public int [] actLevels;
+	private StreamWriter writer;
 
 	// Use this for initialization
 	void Start () {
-
+		writer = new StreamWriter("C:/Users/Adam/Documents/UCF/AI for game programming/trainingData.txt");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		// call this each update to print the training data
 		printTrainingData ();
 	}
 
+	void FixedUpdate () {
+		// call this each update to print the training data
+		//printTrainingData ();
+	}
+
 	void printTrainingData() {
+		writer.WriteLine ("right sensor " + rightSensor.ToString ());
 
 	}
 }
